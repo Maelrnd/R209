8978f01220e44da31e363ba784cb020def768323
Maelrnd <Maelrnd>
2024-04-16 08:44:57 +0200

    update

---
+++ TP2/essai/java.js
@@ -0,0 +1,25 @@
+
+let C=10;
+let F= C * 9/5 + 32
+console.log(C+" degrès");
+console.log(F+" ");
+
+let L=10
+let l=4
+console.log("L'aire du rectengle :"+L*l)
+
+let prenom="lol"
+let nom="ptdr"
+console.log(prenom+" "+nom)
+
+let HT=20
+let prix=10
+console.log("Prix:"+prix+",TVA: "+HT+", "+prix*(1+HT/100))
+
+
+console.log(((4 >= 6) || ("herbe" != "verte")) && !(((12 * 2) == 144) && true))
+
+let poids = 75; 
+let taille = 1.78; 
+let IMC = poids / (taille * taille);
+console.log("L'IMC de la personne est : " + IMC.toFixed(2));
