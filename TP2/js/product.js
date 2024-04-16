d69bdd8899e80860653a59563e6de783c2fdddd0
Maelrnd <Maelrnd>
2024-04-16 11:44:04 +0200

    update

--- TP2/js/product.js
+++ TP2/js/product.js
@@ -1,1 +1,15 @@
 // À vous de jouer !
+
+const currentUrl = new URL(window.location.href);
+const params = new URLSearchParams(currentUrl.search);
+const idParam = params.get('id');
+console.log(idParam)
+fetch(`https://637d41d916c1b892ebca9a5a.mockapi.io/api/discs/${idParam}`)
+    .then(reponse => reponse.json())
+    .then(data => {
+        document.getElementById("cover").innerHTML = `<img src="${data.cover_url}" alt="${data.title}">`
+        document.getElementById("artist").textContent = data.artist
+        document.getElementById("year").textContent = data.year
+        document.getElementById("price").textContent = data.price
+        document.title= data.title
+    })
--- TP2/js/script.js
+++ TP2/js/script.js
@@ -1,1 +1,21 @@
 // À vous de jouer !
+
+const listeAlbums = document.getElementById("items");
+fetch("https://637d41d916c1b892ebca9a5a.mockapi.io/api/discs")
+    .then(reponse => reponse.json())
+    .then(data => {
+        const res = data.filter(item => { return item.id !== "7"});
+        console.table(res)
+        res.forEach(album => {
+            listeAlbums.innerHTML += `
+                <a href="./product.html?id=${album.id}">
+                    <article>
+                        <img src="${album.cover_url}" alt="${album.title}">
+                        <h3 class="productName">${album.title}</h3>
+                        <p class="productDescription">${album.price} €</p>
+                    </article>
+                </a>
+            `
+
+        })
+    })
